65c49722829c8a1db59cd5deb20a2f59f513c7bf
Özgür TÜZGEN <otuzgen@stm.com.tr>
2018-02-07 12:51:08 +0300

    initial layout with map (sidebar)

--- src/app.js
+++ src/app.js
@@ -1,113 +1,123 @@
 var loadApp = function () {
     'use strict';
 
-    var Place = function (id, name, lat, long, category, rating) {
-        this.id = ko.observable(id);
-        this.name = ko.observable(name);
-        this.lat = ko.observable(lat);
-        this.long = ko.observable(long);
-        this.category = ko.observable(category);
-        this.rating = ko.observable(rating);
-    };
-
-    var ViewModel = function (cache) {
-
-        var self = this;
-        self.places = ko.observableArray();
-        self.markers = [];
-        self.selectedItems = ko.observableArray();
-        self.gridOptions = { 
-            displaySelectionCheckbox:false, 
-            data: self.places, 
-            showFilter: false, 
-            showColumnMenu: false, 
-            multiSelect:false, 
-            selectedItems: self.selectedItems,
-            footerVisible: false,
-            enableColumnResize: false
-        };
-        self.filterPlaces = function () {
-            // alert("filter triggered");
-            // self.places.splice(2, 10);
-            alert(self.selectedItems()[0].name());
+        var Place = function (id, name, lat, long, category, rating) {
+            this.id = ko.observable(id);
+            this.name = ko.observable(name);
+            this.lat = ko.observable(lat);
+            this.long = ko.observable(long);
+            this.category = ko.observable(category);
+            this.rating = ko.observable(rating);
         };
 
-        var getPlaces = function () {
-            fetch('https://api.foursquare.com/v2/venues/explore?near=Ankara&oauth_token=3EXVT5GGO1OBN4511E0LPNLFLWAOTYRHLXXRFUVXYGYHD22U&v=20180109')
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (response) {
-                    localStorage.setItem('placesResponse', JSON.stringify(response));
-                    addPlaces(response);
-                })
-                .catch(function (error) {
-                    console.log(new Error(error));
-                });
-        };
+        var ViewModel = function (cache) {
 
-        var addPlaces = function (response) {
-            var items = response.response.groups[0].items.slice(1, 20);
-            items.forEach(element => {
-                var newPlace = new Place(element.venue.id, element.venue.name, element.venue.location.lat, element.venue.location.lng, element.venue.categories[0].name, element.venue.rating);
-                self.places().push(newPlace);
+            var self = this;
+            self.places = ko.observableArray();
+            self.markers = [];
+            self.selectedItems = ko.observableArray();
+            self.gridOptions = { 
+                displaySelectionCheckbox:false, 
+                data: self.places, 
+                showFilter: false, 
+                showColumnMenu: false, 
+                multiSelect:false, 
+                selectedItems: self.selectedItems,
+                footerVisible: false,
+                enableColumnResize: false
+            };
+            self.filterPlaces = function () {
+                // alert("filter triggered");
+                // self.places.splice(2, 10);
+                alert(self.selectedItems()[0].name());
+            };
 
-                // Create a marker per location, and put into markers array.
-                var marker = new google.maps.Marker({
-                    position: element.venue.location,
-                    title: element.venue.name,
-                    id: element.venue.id,
-                    map: map
-                });
+            var getPlaces = function () {
+                fetch('https://api.foursquare.com/v2/venues/explore?near=Ankara&oauth_token=3EXVT5GGO1OBN4511E0LPNLFLWAOTYRHLXXRFUVXYGYHD22U&v=20180109')
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (response) {
+                        localStorage.setItem('placesResponse', JSON.stringify(response));
+                        addPlaces(response);
+                    })
+                    .catch(function (error) {
+                        console.log(new Error(error));
+                    });
+            };
 
-                // Create a single infowindow to be used with the place details information
-                // so that only one is open at once.
-                var placeInfoWindow = new google.maps.InfoWindow();
-                // If a marker is clicked, do a place details search on it in the next function.
-                marker.addListener('click', function () {
+            var addPlaces = function (response) {
+                var items = response.response.groups[0].items.slice(1, 20);
+                items.forEach(element => {
+                    var newPlace = new Place(element.venue.id, element.venue.name, element.venue.location.lat, element.venue.location.lng, element.venue.categories[0].name, element.venue.rating);
+                    self.places().push(newPlace);
 
-                    self.places().forEach((place) => {
-                        if (place.id() == marker.id) {
-                            console.log('found');
-                            placeInfoWindow.marker = marker;
-                            var innerHTML = '<div>';
-                            if (place.name()) {
-                                innerHTML += '<strong>' + place.name() + '</strong>';
-                            }
-                            if (place.category()) {
-                                innerHTML += '<strong>' + place.category() + '</strong>';
-                            }
-
-                            innerHTML += '</div>';
-                            placeInfoWindow.setContent(innerHTML);
-                            placeInfoWindow.open(map, marker);
-                            // Make sure the marker property is cleared if the infowindow is closed.
-                            placeInfoWindow.addListener('closeclick', function () {
-                                placeInfoWindow.marker = null;
-                            });
-                        }
+                    // Create a marker per location, and put into markers array.
+                    var marker = new google.maps.Marker({
+                        position: element.venue.location,
+                        title: element.venue.name,
+                        id: element.venue.id,
+                        map: map
                     });
 
-                });
+                    // Create a single infowindow to be used with the place details information
+                    // so that only one is open at once.
+                    var placeInfoWindow = new google.maps.InfoWindow();
+                    // If a marker is clicked, do a place details search on it in the next function.
+                    marker.addListener('click', function () {
 
-                self.markers.push(marker);
-            });
+                        self.places().forEach((place) => {
+                            if (place.id() == marker.id) {
+                                console.log('found');
+                                placeInfoWindow.marker = marker;
+                                var innerHTML = '<div>';
+                                if (place.name()) {
+                                    innerHTML += '<strong>' + place.name() + '</strong>';
+                                }
+                                if (place.category()) {
+                                    innerHTML += '<strong>' + place.category() + '</strong>';
+                                }
+
+                                innerHTML += '</div>';
+                                placeInfoWindow.setContent(innerHTML);
+                                placeInfoWindow.open(map, marker);
+                                // Make sure the marker property is cleared if the infowindow is closed.
+                                placeInfoWindow.addListener('closeclick', function () {
+                                    placeInfoWindow.marker = null;
+                                });
+                            }
+                        });
+
+                    });
+
+                    self.markers.push(marker);
+                });
+            };
+
+            if (!cache || cache.length < 1) {
+                getPlaces(self.places());
+            }
+            else {
+                addPlaces(cache);
+            }
         };
 
-        if (!cache || cache.length < 1) {
-            getPlaces(self.places());
-        }
-        else {
-            addPlaces(cache);
-        }
-    };
+        // check local storage for places
+        var cache = ko.utils.parseJson(localStorage.getItem('placesResponse'));
 
-    // check local storage for places
-    var cache = ko.utils.parseJson(localStorage.getItem('placesResponse'));
+        // bind a new instance of our view model to the page
+        var viewModel = new ViewModel(cache || []);
+        ko.applyBindings(viewModel);
 
-    // bind a new instance of our view model to the page
-    var viewModel = new ViewModel(cache || []);
-    ko.applyBindings(viewModel);
+    var myWrapper = $("#wrapper");
+    $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+        myWrapper.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function (e) {
+            // code to execute after transition ends
+            google.maps.event.trigger(map, 'resize');
+        });
+    });
 };
 
 var initMap = function () {
@@ -118,7 +128,7 @@ var initMap = function () {
             center: latlng
         };
 
-    map = new google.maps.Map(document.getElementById("mapDiv"), myOptions);
+    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
     loadApp();
 };
